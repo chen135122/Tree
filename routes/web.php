7c7a289f4225fe85ef3c6f66b15de37f80703dd4
DavidNineRoc <1033404553@qq.com>
2018-01-12 14:06:30 +0800

    添加后台首页

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,12 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return 'Hello, World!';
+});
+
+
+Route::prefix('admin')->namespace('Admin')->group(function() {
+
+    Route::get('/', 'HomeController@index');
+
 });
