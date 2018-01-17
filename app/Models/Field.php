10614483c7ede4c9585bce3f457ab5edf67776ef
DavidNineRoc <1033404553@qq.com>
2018-01-17 13:57:41 +0800

    '填充菜单表'

--- app/Models/Field.php
+++ app/Models/Field.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Field extends Model
 {
     public $timestamps = false;
+
+    public function users()
+    {
+        return $this->belongsToMany(User::class);
+    }
 }
